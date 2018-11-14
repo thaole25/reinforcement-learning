efd0e123fa50ef7df7fdb593eff0fb50b041c328
thaole <thao.t.le25@gmail.com>
2018-11-14 15:37:10 +1100

    select action by epsilon greedy

--- frozen.py
+++ frozen.py
@@ -1,9 +1,15 @@
 """
+FrozenLake environment in gym openai
+
 Actions
     - 0: Left
     - 1: Down 
     - 2: Right
     - 3: Up
+
+Rewards:
+    - 1: reach goal
+    - 0: otherwise
 """
 
 import gym
@@ -14,8 +20,11 @@ import random
 ENVIRONMENT = 'FrozenLake8x8-v0'
 ALPHA = 0.4
 DISCOUNT_FACTOR = 0.9
-EPISODES = 500
+EPISODES = 5000
 STEPS = 100
+EPSILON = 0.05
+ALL_ACTIONS = [0,1,2,3]
+QTABLE = True
 
 # Initialise 
 env = gym.make(ENVIRONMENT)
@@ -26,14 +35,18 @@ for eps in range(EPISODES):
     state = env.reset() # Initialise state
     #time.sleep(0.1)
     for step in range(STEPS):
-        # Select the next action from Q(s,a)
-        maxQ = np.max(Q[state, :])
-        actions = np.argwhere(Q[state, :] == maxQ).flatten().tolist()
-        action = random.choice(actions)
+        # Select the next action from Q(s,a): epsilon greedy with tie breaking
+        prob = np.random.random()
+        if prob <= EPSILON:
+            action = random.choice(ALL_ACTIONS)
+        else:
+            maxQ = np.max(Q[state, :])
+            actions = np.argwhere(Q[state, :] == maxQ).flatten().tolist()
+            action = random.choice(actions)
 
         # Execute action and observe new state
         nextState, reward, done, info = env.step(action)
-
+        
         # Update Q
         Q[state, action] += ALPHA * (reward + DISCOUNT_FACTOR * np.max(Q[nextState, :]) - Q[state, action])
         
@@ -43,4 +56,7 @@ for eps in range(EPISODES):
         if done:
             #print("Episode {} finished after {} timesteps".format(eps+1, step+1))
             break
-    env.render()
+    #env.render()
+
+if QTABLE:
+    print (Q)
